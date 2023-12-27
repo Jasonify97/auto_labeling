15acec6eb38845f221090503f7c01bcffe971547
caselab_LJY <wnsdyd1489@gmail.com>
2023-12-27 10:12:20 +0900

    [BE] random folder name library

--- auto_labeling/views.py
+++ auto_labeling/views.py
@@ -1,8 +1,16 @@
+import os, secrets
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.sessions.models import Session
 
 def main(request):
     return render(request, 'main.html')
 
 def start(request):
     return render(request, 'start.html')
+
+def index(request):
+    print(secrets.token_hex())
+
+
+    return HttpResponse('Here is Index View!')
