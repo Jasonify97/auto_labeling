a56d39c294b3fab3b750d288272d6b1865f00075
caselabkya <caselabkya@gmail.com>
2023-12-28 15:04:59 +0900

    Day 2

--- auto_labeling/urls.py
+++ auto_labeling/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls), # 일단 냄겨둠
-    path('start/', views.start, name='start'), #start 페이지로 이동
-    path('', views.main, name='main'),
+    path('', views.start, name='start'), #start 페이지로 이동
+    path('main/', views.main, name='main'),
 ]
