d4cd11f41f9f2d9f5ca158cf104d8019e7417844
caselabkya <caselabkya@gmail.com>
2024-01-03 12:51:43 +0900

    Edit views

--- auto_labeling/views.py
+++ auto_labeling/views.py
@@ -1,4 +1,5 @@
 import os, secrets
+from django.conf import settings
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
@@ -10,13 +11,16 @@ def main(request):
 def start(request):
     return render(request, 'start.html')
 
-def upload_file(request): # /media에 파일 업로드
-    if request.method == 'POST':
-        folder_path = os.path.join("media","img")
-        for image in request.FILES.getlist('file'):
-            destination_path = os.path.join(folder_path, image.name)
-            with open(destination_path, 'wb+') as destination:
-                for chunk in image.chunks():
+def upload_file(request): # /media에 파일 업로드     
+    if request.method == 'POST' and request.FILES.getlist('file'):
+        uploaded_files = request.FILES.getlist('file')
+        upload_dir = os.path.join(settings.MEDIA_ROOT, 'img')
+        if not os.path.exists(upload_dir):
+            os.makedirs(upload_dir)
+        for uploaded_file in uploaded_files:
+            file_path = os.path.join(upload_dir, uploaded_file.name)
+            with open(file_path, 'wb+') as destination:
+                for chunk in uploaded_file.chunks():
                     destination.write(chunk)
         return render(request, 'main.html')
     return HttpResponse('Failed') 
